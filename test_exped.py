89ca0b1d0a6cfcc92f03188a153a66290d6fe113
oshurek_m <max.ashurek@gmil.com>
2021-01-14 18:07:10 +0300

    ккк

--- test_exped.py
+++ test_exped.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_excel('EXPED.xlsb', header=4, usecols='B:AC', engine='pyxlsb', skiprows=[5],nrows=2295)
+df = pd.read_excel('EXPED.xlsb', header=4, usecols='B:AC', engine='pyxlsb', skiprows=[5], nrows=2295)
 print(df.head(10))
 print(df.tail())
 df.info()
