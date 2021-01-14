c90ac0fea24c1565063822d43f65a94cab471e9e
ashurek-m <max.ashurek@gmail.com>
2021-01-14 22:13:09 +0300

    test os v3

--- test_os/test_os.py
+++ test_os/test_os.py
@@ -2,13 +2,13 @@ import os
 
 print(os.getcwd())
 list_dir = os.listdir(path=".")
-print(len(list_dir))
+
 for i in range(len(list_dir)):
     print(list_dir[i])
 
 os.chdir(r"E:\Games")
 print(os.getcwd())
 list_dir = os.listdir(path=".")
-print(len(list_dir))
+
 for i in range(len(list_dir)):
     print(list_dir[i])
