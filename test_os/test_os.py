ee0369c77df2d8605f4835bc92f367f34321d4b7
oshurek_m <max.ashurek@gmil.com>
2021-01-15 17:54:49 +0300

    сделал словарь из номеров заказа из папки с заказами

--- test_os/test_os.py
+++ test_os/test_os.py
@@ -6,9 +6,17 @@ list_dir = os.listdir(path=".")
 for i in range(len(list_dir)):
     print(list_dir[i])
 
-os.chdir(r"E:\Games")
+os.chdir(r"W:\Department - External Economic\ЗАКАЗЫ ВНУТРЕННИХ ПОТРЕБИТЕЛЕЙ\Consultations RTB-2018")
 print(os.getcwd())
 list_dir = os.listdir(path=".")
 
+order_list = []
 for i in range(len(list_dir)):
-    print(list_dir[i])
+    if list_dir[i][0:5].isdigit():
+        order_list.append({
+            'numder_order': list_dir[i][0:5]
+        })
+    # print(list_dir[i].isdigit())
+
+for row in order_list:
+    print(row)
