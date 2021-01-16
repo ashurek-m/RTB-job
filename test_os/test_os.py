fa007889715a263a5f68e4f0d74eb1b21dc5ef71
oshurek_m <max.ashurek@gmil.com>
2021-01-16 16:14:15 +0300

    тест создания csv файла

--- test_os/test_os.py
+++ test_os/test_os.py
@@ -1,4 +1,4 @@
-import os
+import os, glob
 
 print(os.getcwd())
 list_dir = os.listdir(path=".")
@@ -16,11 +16,9 @@ for i in range(len(list_dir)):
         order_list.append({
             'numder_order': list_dir[i][0:5],
             'full_name': list_dir[i],
-            'directory_path': "W:\Department - External Economic\ЗАКАЗЫ ВНУТРЕННИХ ПОТРЕБИТЕЛЕЙ\Consultations RTB-2018\\" + list_dir[i] ,
         })
     # print(list_dir[i].isdigit())
 
-print(order_list[0]['directory_path'])
-print('W:\\Department - External Economic\\ЗАКАЗЫ ВНУТРЕННИХ ПОТРЕБИТЕЛЕЙ\\Consultations RTB-2018\\36885 - Унитехпром БГУ')
-os.chdir(r'W:\\Department - External Economic\\ЗАКАЗЫ ВНУТРЕННИХ ПОТРЕБИТЕЛЕЙ\\Consultations RTB-2018\\36885 - Унитехпром БГУ')
-print('N ',os.getcwd())
+way_list = glob.glob('W:\Department - External Economic\ЗАКАЗЫ ВНУТРЕННИХ ПОТРЕБИТЕЛЕЙ\Consultations RTB-2018\**\*.xls', recursive=True)
+
+print(len(way_list))
