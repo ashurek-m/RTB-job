20e1834bf9d001f7c10f7c92cff12772f5122041
ashurek-m <max.ashurek@gmail.com>
2021-01-17 20:26:40 +0300

    test os v4

--- test_os/test_rec_csv.py
+++ test_os/test_rec_csv.py
@@ -1,6 +1,28 @@
 import csv
-with open('eggs.csv', 'wb') as csvfile:
+with open('eggs.csv', 'a') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                            quotechar=',', quoting=csv.QUOTE_MINIMAL)
     spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
     spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+    csvfile.close()
+
+
+def csv_writer(data, path):
+    """
+    Write data to a CSV file path
+    """
+    with open(path, "w", newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for line in data:
+            writer.writerow(line)
+
+
+if __name__ == "__main__":
+    data = ["first_name,last_name,city".split(","),
+            "Tyrese,Hirthe,Strackeport".split(","),
+            "Jules,Dicki,Lake Nickolasville".split(","),
+            "Dedric,Medhurst,Stiedemannberg".split(",")
+            ]
+
+    path = "output.csv"
+    csv_writer(data, path)
