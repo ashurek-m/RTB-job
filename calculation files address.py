0f0087b2dca1f300d4915fa92b4cc13793ec91a2
oshurek_m <max.ashurek@gmil.com>
2021-01-20 17:32:14 +0300

    редактирование

--- calculation files address.py
+++ calculation files address.py
@@ -12,13 +12,11 @@ def csv_writer(data, path):
     Write data to a CSV file path
     """
     with open(path, "w", encoding='utf-8', newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',')
+        writer = csv.writer(csv_file)
         writer.writerows(data)
         csv_file.close()
 
 #encoding='utf-8'
 way_list = glob.glob('W:\Department - External Economic\ЗАКАЗЫ ВНУТРЕННИХ ПОТРЕБИТЕЛЕЙ\Consultations RTB-2018\**\*.xls', recursive=True)
-path = 'address_file.csv'
-
+path = "address_file.csv"
 csv_writer(spisok_spiskov(way_list), path)
-
