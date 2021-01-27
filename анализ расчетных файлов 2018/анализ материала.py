fa2125a3b528ff085b14475870c9f964290a78a3
ashurek-m <max.ashurek@gmail.com>
2021-01-27 21:21:49 +0300

    улутшение анализа материала.

--- анализ расчетных файлов 2018/анализ материала.py
+++ анализ расчетных файлов 2018/анализ материала.py
@@ -2,11 +2,27 @@ import pandas as pd
 import time, csv
 import xlrd
 
+def spisok_spiskov(data):
+    new_list = []
+    for i in range(len(data)):
+       listys = [data[i]]
+       new_list.append(listys)
+    return new_list
+
+def csv_writer(data, path):
+    with open(path, "w", encoding='utf-8', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerows(data)
+        csv_file.close()
+
 start_time = time.time()
-material = pd.read_csv(r"C:\Python's_project\RTB-job\анализ расчетных файлов 2018\info_mat.csv")
-locmat = pd.read_excel(r"C:\Python's_project\RTB-job\анализ расчетных файлов 2018\Копия справочник материалов LOCMAT.xls",
+material = pd.read_csv("info_mat.csv")
+locmat = pd.read_excel("Копия справочник материалов LOCMAT.xls",
                        sheet_name='locmat')
 print(material.duplicated(subset='наименование_материала').sum())
 print(len(material['наименование_материала'].unique()))
+#ynic = material['наименование_материала'].unique()
+#path = 'уникальные_наименования_материалов.csv'
+#csv_writer(spisok_spiskov(ynic), path)
 
 print("--- %s seconds ---" % (time.time() - start_time))
