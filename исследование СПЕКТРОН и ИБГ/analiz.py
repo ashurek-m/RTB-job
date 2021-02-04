66ccb6c582e747a201824cf936d8318ccfcbbcaf
oshurek_m <max.ashurek@gmil.com>
2021-02-04 18:23:32 +0300

    создание функции по поиску расчетных файлов по имени клиента

--- исследование СПЕКТРОН и ИБГ/analiz.py
+++ исследование СПЕКТРОН и ИБГ/analiz.py
@@ -13,16 +13,27 @@ def csv_writer(data, path):
         csv_file.close()
 
 
-start_time = time.time()
-csv1 = pd.read_csv("good_address_file.csv", names=['path'])
-name_list = []
-order_number = []
-file = csv1.loc[:, 'path']
-for i in range(len(file)):
-    if 'IBG' in file[i]:
-        name_file = file[i].split("\\")
-        name_list.append(name_file)
-        order_number.append(name_file[-1][:5])
+def search_by_client(address_file, client):
+    file_csv = pd.read_csv(address_file, names=['path'])
+    name_client = []
+    order_number_client = []
+    address_file_client = []
+    file = file_csv.loc[:, 'path']
+    for i in range(len(file)):
+        if client in file[i]:
+            name_file = file[i].split("\\")
+            try:
+                name_client.append(name_file)
+                num_order = int(name_file[-1][:5])
+                order_number_client.append(num_order)
+                address_file_client.append(file[i])
+            except ValueError:
+                continue
+    print(name_client[0], address_file_client[0])
+    print(order_number_client)
 
-print(name_list[0])
-print(order_number[0])
+
+start_time = time.time()
+search_by_client('good_address_file.csv', 'IBG')
+
+print("--- %s seconds ---" % (time.time() - start_time))
