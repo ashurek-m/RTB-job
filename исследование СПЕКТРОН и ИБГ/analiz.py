5eba106e64607608c4cde8bc03217bf672f53b25
oshurek_m <max.ashurek@gmil.com>
2021-02-08 21:43:54 +0300

    алгоритм сброса расетных файлов в один файл

--- исследование СПЕКТРОН и ИБГ/analiz.py
+++ исследование СПЕКТРОН и ИБГ/analiz.py
@@ -1,41 +1,35 @@
+import csv
+import time
 import pandas as pd
-import time, csv
 
 
 def csv_writer(data, path):
-    new_list = []
-    for i in range(len(data)):
-        listys = [data[i]]
-        new_list.append(listys)
     with open(path, "w", encoding='utf-8', newline='') as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerows(new_list)
+        writer.writerows(data)
         csv_file.close()
 
 
 def search_by_client(address_file, client):
     file_csv = pd.read_csv(address_file, names=['path'])
-    file_adrress_client = []
-    order_number_client = []
     address_file_client = []
     file = file_csv.loc[:, 'path']
     for i in range(len(file)):
+        file_adrress_client = []
         registr = file[i].title()
         if client in registr:
             name_file = file[i].split("\\")
             try:
-                name_client.append(name_file)
                 num_order = int(name_file[-1][:5])
                 file_adrress_client.append(file[i])
-                order_number_client.append(num_order)
-                address_file_client.append(file[i])
+                file_adrress_client.append(num_order)
+                address_file_client.append(file_adrress_client)
             except ValueError:
                 continue
     path = client + '.csv'
-    csv_writer(file_adrress_client, path)
+    csv_writer(address_file_client, path)
 
 
 start_time = time.time()
-search_by_client('good_address_file.csv', 'Ibg')
-
+search_by_client('good_address_file.csv', 'Спектрон')
 print("--- %s seconds ---" % (time.time() - start_time))
--- исследование СПЕКТРОН и ИБГ/сбор данных из расчетного файла.py
+++ исследование СПЕКТРОН и ИБГ/сбор данных из расчетного файла.py
@@ -3,18 +3,13 @@ import time
 
 
 start_time = time.time()
-address_file = pd.read_csv("Ibg.csv", names=['path'])
+address_file = pd.read_csv("Спектрон.csv", names=['path', 'order'])
 df = address_file.loc[:, 'path']
-writer = pd.ExcelWriter('Ibg.xlsx', engine='xlsxwriter')
 for i in range(len(df)):
-    if i == 0:
-        payment_file = pd.read_excel(df[i], sheet_name='расчет', header=13)
-        payment_file.to_excel(writer, sheet_name='welcome', index=False)
-        writer.save()
-    else:
-        payment_file = pd.read_excel(df[i], sheet_name='расчет', header=13)
-        payment_file.to_excel(writer, sheet_name='welcome', index=False)
+    payment_file = pd.read_excel(df[i], sheet_name='расчет', header=13)
+    print(payment_file.shape, address_file.loc[i, 'order'])
 
-writer.save()
+
+
 print("--- %s seconds ---" % (time.time() - start_time))
 
