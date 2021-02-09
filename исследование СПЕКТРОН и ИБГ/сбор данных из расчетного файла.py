d00a2e070e22861b8f65a8e8ea9d246af605ece7
oshurek_m <max.ashurek@gmil.com>
2021-02-09 22:11:08 +0300

    алгоритм сброса расетных файлов в один файл 1

--- исследование СПЕКТРОН и ИБГ/сбор данных из расчетного файла.py
+++ исследование СПЕКТРОН и ИБГ/сбор данных из расчетного файла.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import time
+import csv
+import numpy as np
+
+
+def csv_writer(data, path):
+    with open(path, "w", encoding='utf-8', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerows(data)
+        csv_file.close()
 
 
 start_time = time.time()
@@ -12,7 +21,23 @@ for i in range(len(df)):
 
 df_shape = pd.DataFrame(data=shape_list, columns=['кол-во_столбцов'])
 
+list_columns = []
+for i in range(len(df)):
+    payment_file = pd.read_excel(df[i], sheet_name='расчет', header=13)
+    x_shape = payment_file.shape[1]
+    if x_shape == shape_list[0]:
+        list_columns.append(payment_file.columns)
+csv_writer(list_columns, 'columns.csv')
 
-
+matrix_a = np.array(list_columns)
+matrix_b = np.transpose(matrix_a)
+atr_s = 0
+for i in range(matrix_b.shape[0]):
+    atr_a = 0
+    for j in range(matrix_b.shape[1] - 1):
+        if matrix_b[i][j] == matrix_b[i][j + 1]:
+            atr_a += 1
+    if atr_a == matrix_b.shape[1] - 1:
+        atr_s += 1
+print(atr_s == matrix_b.shape[0])
 print("--- %s seconds ---" % (time.time() - start_time))
-
