6f27dfa4132cd1d697f2f2e4ae4b9d1ae371cc7c
oshurek_m <max.ashurek@gmil.com>
2021-02-12 18:20:44 +0300

    алгоритм сброса расетных файлов в один файл 3

--- исследование СПЕКТРОН и ИБГ/сбор данных из расчетного файла.py
+++ исследование СПЕКТРОН и ИБГ/сбор данных из расчетного файла.py
@@ -12,15 +12,19 @@ def csv_writer(data, path):
 
 
 start_time = time.time()
+# Сделали список где содержится кол-во столбцов в расчетном файле
+# Для того что бы рассортировать схожие файлы по конфигурации
 address_file = pd.read_csv("Спектрон.csv", names=['path', 'order'])
 df = address_file.loc[:, 'path']
 shape_list = []
 for i in range(len(df)):
     payment_file = pd.read_excel(df[i], sheet_name='расчет', header=13)
     shape_list.append(payment_file.shape[1])
-
+# Преобразовали список в датафрейм
 df_shape = pd.DataFrame(data=shape_list, columns=['кол-во_столбцов'])
-
+# Создаем список с названием столбцов для всех расчетных файлов с одинаковым кол-вом столбцов
+# Сохраняем результат в csv файл для просмотра
+# Рассматриваем пока конкретно по одному варианту
 list_columns = []
 for i in range(len(df)):
     payment_file = pd.read_excel(df[i], sheet_name='расчет', header=13)
@@ -28,9 +32,10 @@ for i in range(len(df)):
     if x_shape == shape_list[0]:
         list_columns.append(payment_file.columns)
 csv_writer(list_columns, 'columns.csv')
-
+# Из списка создаем матрицу и транспонируем ее.
 matrix_a = np.array(list_columns)
 matrix_b = np.transpose(matrix_a)
+# Проверяем совподают ли наименования колонок в расчетных файлах с одинаковым кол-вом колонок
 atr_s = 0
 for i in range(matrix_b.shape[0]):
     atr_a = 0
@@ -39,5 +44,7 @@ for i in range(matrix_b.shape[0]):
             atr_a += 1
     if atr_a == matrix_b.shape[1] - 1:
         atr_s += 1
-print(atr_s == matrix_b.shape[0])
+output = atr_s == matrix_b.shape[0]
+# Выводим на экран результат проверки True - все совподает, False - не совподает, необходимо определить что не совподает
+print(output)
 print("--- %s seconds ---" % (time.time() - start_time))
