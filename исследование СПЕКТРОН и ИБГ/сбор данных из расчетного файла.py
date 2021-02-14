6ccb43fa1cc0541c2ed17d867e7b0e6270aa74c4
oshurek_m <max.ashurek@gmil.com>
2021-02-14 17:00:43 +0300

    алгоритм сброса расетных файлов в один файл 8

--- исследование СПЕКТРОН и ИБГ/сбор данных из расчетного файла.py
+++ исследование СПЕКТРОН и ИБГ/сбор данных из расчетного файла.py
@@ -13,57 +13,61 @@ def csv_writer(data, path):
 
 def save_excel(name_fale, data_frame, columns=True):
     writer_on = pd.ExcelWriter(name_fale, engine='xlsxwriter')
-    data_frame.to_excel(writer_on, sheet_name='welcome', index=False, header=columns)
+    data_frame.to_excel(writer_on, index=False, header=columns)
     writer_on.save()
 
 
 start_time = time.time()
-# Сделали список где содержится кол-во столбцов в расчетном файле
-# Для того что бы рассортировать схожие файлы по конфигурации
-address_file = pd.read_csv("Спектрон.csv", names=['path', 'order'])
-df = address_file.loc[:, 'path']
-df_order = address_file.loc[:, 'order']
-shape_list = []
-for i in range(len(df)):
-    payment_file = pd.read_excel(df[i], sheet_name='расчет', header=13)
-    payment_file = payment_file.loc[:, 'Unnamed: 0': 'час']
-    shape_list.append(payment_file.shape[1])
-# Преобразовали список в датафрейм
-df_shape = pd.DataFrame(data=shape_list, columns=['кол-во_столбцов'])
-unique_shape = df_shape['кол-во_столбцов'].unique()
-print(df_shape['кол-во_столбцов'].unique())
-# Создаем список с названием столбцов для всех расчетных файлов с одинаковым кол-вом столбцов
-# Сохраняем результат в csv файл для просмотра
-# Рассматриваем пока конкретно по одному варианту
-list_columns = []
-for i in range(len(df)):
-    payment_file = pd.read_excel(df[i], sheet_name='расчет', header=13)
-    payment_file = payment_file.loc[:, 'Unnamed: 0': 'час']
-    x_shape = payment_file.shape[1]
-    if x_shape == unique_shape[5]:
-        print(df_order[i])
-        list_columns.append(payment_file.columns)
-csv_writer(list_columns, 'columns.csv')
-# Из списка создаем матрицу и транспонируем ее.
-matrix_a = np.array(list_columns)
-matrix_b = np.transpose(matrix_a)
-# Проверяем совподают ли наименования колонок в расчетных файлах с одинаковым кол-вом колонок
-atr_s = 0
-for i in range(matrix_b.shape[0]):
-    atr_a = 0
-    for j in range(matrix_b.shape[1] - 1):
-        if matrix_b[i][j] == matrix_b[i][j + 1]:
-            atr_a += 1
-        else:
-            print(f'строка = {i}, значение({matrix_b[i][j]}, {matrix_b[i][j + 1]})')
-    if atr_a == matrix_b.shape[1] - 1:
-        atr_s += 1
-output = atr_s == matrix_b.shape[0]
-# Выводим на экран результат проверки True - все совподает, False - не совподает, необходимо определить что не совподает
-print(output)
+runner = 60
+if runner != 60:
+    # Сделали список где содержится кол-во столбцов в расчетном файле
+    # Для того что бы рассортировать схожие файлы по конфигурации
+    address_file = pd.read_csv("Спектрон.csv", names=['path', 'order'])
+    df = address_file.loc[:, 'path']
+    df_order = address_file.loc[:, 'order']
+    shape_list = []
+    for i in range(len(df)):
+        payment_file = pd.read_excel(df[i], sheet_name='расчет', header=13)
+        payment_file = payment_file.loc[:, 'Unnamed: 0': 'час']
+        shape_list.append(payment_file.shape[1])
+    # Преобразовали список в датафрейм
+    df_shape = pd.DataFrame(data=shape_list, columns=['кол-во_столбцов'])
+    unique_shape = df_shape['кол-во_столбцов'].unique()
+    print(df_shape['кол-во_столбцов'].unique())
+    # Создаем список с названием столбцов для всех расчетных файлов с одинаковым кол-вом столбцов
+    # Сохраняем результат в csv файл для просмотра
+    # Рассматриваем пока конкретно по одному варианту
+    list_columns = []
+    for i in range(len(df)):
+        payment_file = pd.read_excel(df[i], sheet_name='расчет', header=13)
+        payment_file = payment_file.loc[:, 'Unnamed: 0': 'час']
+        x_shape = payment_file.shape[1]
+        if x_shape == unique_shape[5]:
+            print(df_order[i])
+            list_columns.append(payment_file.columns)
+    csv_writer(list_columns, 'columns.csv')
+    # Из списка создаем матрицу и транспонируем ее.
+    matrix_a = np.array(list_columns)
+    matrix_b = np.transpose(matrix_a)
+    # Проверяем совподают ли наименования колонок в расчетных файлах с одинаковым кол-вом колонок
+    atr_s = 0
+    for i in range(matrix_b.shape[0]):
+        atr_a = 0
+        for j in range(matrix_b.shape[1] - 1):
+            if matrix_b[i][j] == matrix_b[i][j + 1]:
+                atr_a += 1
+            else:
+                print(f'строка = {i}, значение({matrix_b[i][j]}, {matrix_b[i][j + 1]})')
+        if atr_a == matrix_b.shape[1] - 1:
+            atr_s += 1
+    output = atr_s == matrix_b.shape[0]
+    # Выводим на экран результат проверки True - все совподает, False - не совподает, необходимо определить что не
+    # совподает
+    print(output)
+
 # Группируем расчетные файлы с одинаковым кол-вом столбоц в подгенеральныей файлы. Это делается из-за разного
 # наименования столбов в файлах. Хотя можно было присвоить свои наименования столбов в каждом файле.
-runner = 60
+
 if runner == 66:
     counter = 0
     for i in range(len(df)):
