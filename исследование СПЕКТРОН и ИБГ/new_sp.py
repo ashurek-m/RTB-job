2edf6537ed82c52d276a26242519228f4f0d9b9c
oshurek_m <max.ashurek@gmil.com>
2021-03-12 18:20:04 +0300

    анализ СПЕКТРОН РТБ 1

--- исследование СПЕКТРОН и ИБГ/new_sp.py
+++ исследование СПЕКТРОН и ИБГ/new_sp.py
@@ -4,15 +4,26 @@ import time
 
 start_time = time.time()
 df_pay = pd.read_excel('спектронус-плюс.xlsx')
+df_pay =df_pay.assign(dart_prepa=lambda x: x['dart_т_подг_мин'] + x['mx\dart-tour_т_подг_мин'])
+df_pay = df_pay.assign(dart_catting=lambda x: x['dart_т_маш_мин'] + x['mx\dart-tour_т_ман_мин'])
 df_tech = pd.read_excel('АРХИВ ТЕХНОЛОГИЙ плюс.xlsx')
 iter = df_pay.shape[0]
+time_list = []
 print(iter)
 for j in range(iter):
     df_tech1 = df_tech[df_tech['name_3'] == df_pay.loc[j, 'name_2']]
     df_tech2 = df_tech1[df_tech1['ИНДЕКС'] == df_pay.loc[j, 'index_2']]
-    print(df_pay.loc[j, 'name_2'])
-    print(df_tech2.loc[:, 'Tpc prepa': 'Tpc exec'])
-    t_cn_prepa = df_tech2.groupby('Tache')['Tpc prepa'].sum()
-    print(t_cn_prepa)
+    time_prepa = df_tech2.groupby('Tache')['Tpc prepa'].sum()
+    time_catting = df_tech2.groupby('Tache')['Tpc exec'].sum()
+    print(time_prepa)
+    print(time_catting)
+    df_pay1 = df_pay.loc[j, ['cn_т_подг_мин',
+                             'cn_т_маш_мин',
+                             'dart_prepa',
+                             'dart_catting',
+                             'tour_т_подг_мин',
+                             'tour_т_маш_мин']]
+    print(df_pay1)
+    print('stop')
     input()
 print("--- %s seconds ---" % (time.time() - start_time))
