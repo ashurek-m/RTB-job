e36ee8123cf2348260b631341a8a6612a9dbd559
oshurek_m <max.ashurek@gmil.com>
2021-03-31 18:06:15 +0300

    единый расчетный файл 15

--- создание единого расчетного файла/привязка технологий.py
+++ создание единого расчетного файла/привязка технологий.py
@@ -6,52 +6,60 @@ def save_csv_header_w(path, data_frame, columns=True, w_or_a='w'):
     data_frame.to_csv(path, mode=w_or_a, encoding='utf-8', index=False, header=columns)
 
 
+def transformation_pay(path):
+    df_pay_data = pd.read_excel(path)
+    df_cod_baan = df_pay_data.loc[:, 'обозначение']
+    cod_baan = []
+    for i in range(len(df_cod_baan)):
+        format_cod_baan = df_cod_baan[i].strip()
+        one_cod_baan = [x for x in format_cod_baan]
+        cod_baan.append(one_cod_baan)
+    df_full_cod = []
+    for i in range(len(cod_baan)):
+        full_cod = [x for x in cod_baan[i] if x.isalpha() == False]
+        df_full_cod.append(''.join(full_cod))
+    df_pay_data = df_pay_data.assign(full_cod1=df_full_cod)
+    save_csv_header_w('full_pay_2020.csv', df_pay_data)
+
+
+def transformation_table_courant(path):
+    df_table = pd.read_excel(path)
+    df_cod_baan = df_table.loc[:, 'REF_BAAN']
+    cod_baan = []
+    for i in range(len(df_cod_baan)):
+        format_cod_baan = str(df_cod_baan[i]).strip()
+        one_cod_baan = [x for x in format_cod_baan]
+        cod_baan.append(one_cod_baan)
+    df_full_cod = []
+    for i in range(len(cod_baan)):
+        full_cod = [x for x in cod_baan[i] if x.isalpha() == False]
+        df_full_cod.append(''.join(full_cod))
+    print(len(cod_baan) == len(df_full_cod))
+    df_table = df_table.assign(full_cod3=df_full_cod)
+    save_csv_header_w('table_arch.csv', df_table)
+
+
+def transformation_tech(path):
+    df_tech_data = pd.read_excel(path)
+    df_cod_baan = df_tech_data.loc[:, 'REF_BAAN']
+    cod_baan = []
+    for i in range(len(df_cod_baan)):
+        format_cod_baan = str(df_cod_baan[i]).strip()
+        one_cod_baan = [x for x in format_cod_baan]
+        cod_baan.append(one_cod_baan)
+    df_full_cod = []
+    for i in range(len(cod_baan)):
+        full_cod = [x for x in cod_baan[i] if x.isalpha() == False]
+        df_full_cod.append(''.join(full_cod))
+    print(len(cod_baan) == len(df_full_cod))
+    df_tech_data = df_tech_data.assign(full_cod2=df_full_cod)
+    save_csv_header_w('full_tech.csv', df_tech_data)
+
+
 start_time = time.time()
 a = 's pam123.456-01'
 b = [x for x in a if x.isalpha() == False]
 print(b)
-df_pay_data = pd.read_excel('united_pay_file_2019.xlsx')
-df_cod_baan = df_pay_data.loc[:, 'обозначение']
-cod_baan = []
-for i in range(len(df_cod_baan)):
-    format_cod_baan = df_cod_baan[i].strip()
-    one_cod_baan = [x for x in format_cod_baan]
-    cod_baan.append(one_cod_baan)
-df_full_cod = []
-for i in range(len(cod_baan)):
-    full_cod = [x for x in cod_baan[i] if x.isalpha() == False]
-    df_full_cod.append(''.join(full_cod))
-df_pay_data = df_pay_data.assign(full_cod1=df_full_cod)
-save_csv_header_w('full_pay_2019.csv', df_pay_data)
-
-df_table = pd.read_excel('TABLE ARCHIVE.xlsx')
-df_cod_baan = df_table.loc[:, 'REF_BAAN']
-cod_baan = []
-for i in range(len(df_cod_baan)):
-    format_cod_baan = str(df_cod_baan[i]).strip()
-    one_cod_baan = [x for x in format_cod_baan]
-    cod_baan.append(one_cod_baan)
-df_full_cod = []
-for i in range(len(cod_baan)):
-    full_cod = [x for x in cod_baan[i] if x.isalpha() == False]
-    df_full_cod.append(''.join(full_cod))
-print(len(cod_baan) == len(df_full_cod))
-df_table = df_table.assign(full_cod3=df_full_cod)
-save_csv_header_w('table_arch.csv', df_table)
-
-df_tech_data = pd.read_excel('АРХИВ ТЕХНОЛОГИЙ.xlsx')
-df_cod_baan = df_tech_data.loc[:, 'REF_BAAN']
-cod_baan = []
-for i in range(len(df_cod_baan)):
-    format_cod_baan = str(df_cod_baan[i]).strip()
-    one_cod_baan = [x for x in format_cod_baan]
-    cod_baan.append(one_cod_baan)
-df_full_cod = []
-for i in range(len(cod_baan)):
-    full_cod = [x for x in cod_baan[i] if x.isalpha() == False]
-    df_full_cod.append(''.join(full_cod))
-print(len(cod_baan) == len(df_full_cod))
-df_tech_data = df_tech_data.assign(full_cod2=df_full_cod)
-save_csv_header_w('full_tech.csv', df_tech_data)
+transformation_pay('united_pay_file_2020.xlsx')
 
 print("--- %s seconds ---" % (time.time() - start_time))
