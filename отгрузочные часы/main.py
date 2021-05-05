ffbe8c03fa9f1b83d23768eed5333fa04505df7c
oshurek_m <max.ashurek@gmil.com>
2021-05-05 20:14:27 +0300

    отгруженные часы 3

--- отгрузочные часы/main.py
+++ отгрузочные часы/main.py
@@ -8,4 +8,9 @@ if __name__ in '__main__':
     print('2')
     path_2 = s1.search_by_client(path_1[0], 2021)
     print('3')
-    s2.list_shape_fyn(path_2)
+    path_3 = s2.list_shape_fyn(path_2)
+    print('4')
+    shape_list = s2.analysis(path_3)
+    print('5')
+    for numder in shape_list:
+        s2.shape_(path_3, numder)
--- отгрузочные часы/step_2.py
+++ отгрузочные часы/step_2.py
@@ -28,3 +28,38 @@ def list_shape_fyn(list_addres):
     s1.csv_writer_spisok(not_found_list, 'нет файлов.csv')
     s1.csv_writer(shape_list_2, 'открылись.csv')
     return 'открылись.csv'
+
+
+def analysis(data_file):
+    df = pd.read_csv(data_file, names=['path', 'order', 'shape'])
+    list_shape = df['shape'].unique()
+    print(list_shape)
+    return list_shape
+
+
+def save_excel(path, data_frame, columns):
+    writer_on = pd.ExcelWriter(path, engine='xlsxwriter')
+    data_frame.to_excel(writer_on, index=False, header=columns)
+    writer_on.save()
+
+
+def shape_(address_file, number):
+    counter = 0
+    name_file_csv = 'shape_' + str(number) + '.csv'
+    name_file_exel = 'shape_' + str(number) + '.xlsx'
+    address_data = pd.read_csv(address_file, names=['path', 'order', 'shape'])
+    address_data = address_data[address_data['shape'] == number].reset_index(drop=True)
+    for i in range(address_data.shape[0]):
+        payment_file = pd.read_excel(address_data.loc[i, 'path'], sheet_name='расчет', header=13)
+        payment_file_1 = payment_file.loc[:, 'Unnamed: 1': 'час']
+        payment_file_1 = payment_file_1.assign(order=address_data.loc[i, 'order'])
+        payment_file_1.dropna(subset=['Unnamed: 1'], inplace=True)
+        if counter == 0:
+            counter += 1
+            payment_file_1.to_csv(name_file_csv, mode='w', encoding='utf-8', index=False, header=True)
+        else:
+            payment_file_1.to_csv(name_file_csv, mode='a', encoding='utf-8', index=False, header=False)
+    save_excel = pd.read_csv(name_file_csv)
+    writer = pd.ExcelWriter(name_file_exel, engine='xlsxwriter')
+    save_excel.to_excel(writer, sheet_name='расчет', index=False)
+    writer.save()
