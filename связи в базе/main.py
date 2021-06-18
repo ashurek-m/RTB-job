8d9c4aa21a42997ec154bb09672a99c63043ba59
oshurek_m <max.ashurek@gmil.com>
2021-06-18 17:10:11 +0300

    связи 0

--- связи в базе/main.py
+++ связи в базе/main.py
@@ -1,2 +1,12 @@
+import pandas as pd
+
+
 if __name__ in '__main__':
     print('1')
+    columns = ['det', 'ind', 'count']
+    data = [['1', '1', 15], ['2', '3', 21], ['1', '1', 52], ['2', '2', 33]]
+    df = pd.DataFrame(data=data, columns=columns)
+    df = df[(df['count'] > 0) & (df['count'] < 1000)]
+    df = df.groupby('det', 'ind').size().reset_index('count')
+    print(df)
+
