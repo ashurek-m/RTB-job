0fe69addeee32694c9eb33c8a26c7035b5af511e
oshurek_m <max.ashurek@gmil.com>
2022-03-09 12:28:34 +0300

    12

--- Мат модель/model.py
+++ Мат модель/model.py
@@ -1,19 +1,39 @@
 from pulp import LpMaximize, LpProblem, LpStatus, lpSum, LpVariable
 import pandas as pd
 
+
+def save_excel(path, data_frame, columns=True):
+    writer_on = pd.ExcelWriter(path, engine='xlsxwriter')
+    data_frame.to_excel(writer_on, index=False, header=columns)
+    writer_on.save()
+
+
 df = pd.read_excel('C:\\Users\\oshurek_m\\Desktop\\Пеленг общий.xlsx', sheet_name='matmod')
 df_1 = df[['Индекс RTB', 'Наименование детали', 'Кол-во', 'N° ITEM', 'Стоимость, $', 'CN', 'DART', 'TOUR']]
 df_2 = df_1.dropna(axis=0)
 df_2.info()
-print(df_2.head())
 model = LpProblem('prod', LpMaximize)
-names_det = list(df_2['Наименование детали'])
+names_det = list(df_2['N° ITEM'])
+print(len(names_det))
 costs = dict(zip(names_det, df_2['Стоимость, $']))
 cn = dict(zip(names_det, df_2['CN']))
 dart = dict(zip(names_det, df_2['DART']))
 tour = dict(zip(names_det, df_2['TOUR']))
 
 vat = LpVariable.dicts('vat', names_det, cat='Binary')
+
+costs_1  = []
+for n in vat:
+    costs_1.append(n)
+print(costs_1)
+cost_s1 = pd.Series(data=costs_1, name='N° ITEM')
+cost_df = cost_s1.to_frame()
+cost_df.info()
+cost_2 = []
+for k in vat:
+    cost_2.append(vat[k])
+cost_df = cost_df.assign(re_name=cost_2)
+
 model += lpSum(costs[i]*vat[i] for i in names_det)
 t_cn = 350
 t_dart = 20
@@ -21,14 +41,20 @@ t_tour = 100
 model += lpSum(cn[i]*vat[i] for i in names_det) <= 350
 model += lpSum(dart[i]*vat[i] for i in names_det) <= 20
 model += lpSum(tour[i]*vat[i] for i in names_det) <= 100
-print(type(model))
-model.solver()
-'''print("Status:", LpStatus[model.status])
-for v in model.variables():
-    if v.varValue > 0:
-        print(v.name, "=", v.varValue)
-    else:
-        print(v.name, "=", v.varValue)'''
+model.solve()
+var_list = []
+print("Status:", LpStatus[model.status])
 
+for v in model.variables():
+    var_list_2 = []
+    var_list_2.append(v.name)
+    var_list_2.append(v.varValue)
+    var_list.append(var_list_2)
+
+df_var = pd.DataFrame(data=var_list, columns=['var_name', 'var'])
+df_var.info()
+cost_df.info()
+#df_ex = df_var.merge(cost_df, how='outer', on='re_name')
+#save_excel('C:\\Users\\oshurek_m\\Desktop\\matmod.xlsx', df_ex)
 '''https://proglib.io/p/lineynoe-programmirovanie-praktika-resheniya-zadach-optimizacii-na-python-2020-11-26
 https://www.machinelearningmastery.ru/linear-programming-and-discrete-optimization-with-python-using-pulp-449f3c5f6e99/'''
