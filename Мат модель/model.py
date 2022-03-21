2be1272bcd38b7ceb8bb82777cec2547c4adf58c
oshurek_m <max.ashurek@gmil.com>
2022-03-21 13:35:30 +0300

    добавление в модель ограничения "одна деталь - один станок"

--- Мат модель/model.py
+++ Мат модель/model.py
@@ -12,6 +12,10 @@ df = pd.read_excel('C:\\Users\\oshurek_m\\Desktop\\Пеленг общий.xlsx'
 df_1 = df[['Индекс RTB', 'Наименование детали', 'Кол-во', 'N° ITEM', 'Стоимость, $', 'CN', 'DART', 'TOUR']]
 df_2 = df_1.dropna(axis=0)
 df_2.info()
+# величина смены в часах
+b = 8 * 0.8
+# кол-во смен в месяце
+c = 42
 model = LpProblem('prod', LpMaximize)
 names_det = list(df_2['N° ITEM'])
 print(len(names_det))
@@ -22,7 +26,7 @@ tour = dict(zip(names_det, df_2['TOUR']))
 
 vat = LpVariable.dicts('vat', names_det, cat='Binary')
 
-costs_1  = []
+costs_1 = []
 for n in vat:
     costs_1.append(n)
 print(costs_1)
@@ -41,14 +45,16 @@ t_tour = 100
 model += lpSum(cn[i]*vat[i] for i in names_det) <= 350
 model += lpSum(dart[i]*vat[i] for i in names_det) <= 20
 model += lpSum(tour[i]*vat[i] for i in names_det) <= 100
+for i in names_det:
+    model += (cn[i]*vat[i]) <= c * b
+    model += (dart[i]*vat[i]) <= c * b
+    model += (tour[i]*vat[i]) <= c * b
 model.solve()
 var_list = []
 print("Status:", LpStatus[model.status])
 
 for v in model.variables():
-    var_list_2 = []
-    var_list_2.append(v.name)
-    var_list_2.append(v.varValue)
+    var_list_2 = [v.name, v.varValue]
     var_list.append(var_list_2)
 
 df_var = pd.DataFrame(data=var_list, columns=['re_name', 'var'])
@@ -58,5 +64,6 @@ df = df_var.merge(cost_df, on='re_name', how='outer')
 
 save_excel('C:\\Users\\oshurek_m\\Desktop\\matmod.xlsx', df)
 
-'''https://proglib.io/p/lineynoe-programmirovanie-praktika-resheniya-zadach-optimizacii-na-python-2020-11-26
-https://www.machinelearningmastery.ru/linear-programming-and-discrete-optimization-with-python-using-pulp-449f3c5f6e99/'''
+'''https://proglib.io/p/lineynoe-programmirovanie-praktika-resheniya-zadach-optimizacii-na-python-2020-11-26 
+https://www.machinelearningmastery.ru/linear-programming-and-discrete-optimization-with-python-using-pulp
+-449f3c5f6e99/ '''
